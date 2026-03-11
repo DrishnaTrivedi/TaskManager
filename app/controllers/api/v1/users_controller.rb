01daae28605f617d6b9bc2c700bf75b2268f881f
DrishnaTrivedi <drishna4505@gmail.com>
2026-03-11 16:57:25 +0530

    Api endpoints for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::UsersController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def create 
     user = User.new(user_params)
     if user.save
@@ -7,10 +10,34 @@ class Api::V1::UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
+
+  def show
+    render json: @user, status: :ok  
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      render json: {message: "User with name #{@user.name} has been deleted"}, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
   def user_params
     params.expect(user: [:name, :email, :password])
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
