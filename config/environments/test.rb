5a14411ecf16fce4d7ff15afde16a69d6ef58fa5
Drishna Trivedi <drishna4505@gmail.com>
2026-03-23 11:36:40 +0530

    Added tests for mailing services

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,4 +3,6 @@ Rails.application.configure do
   config.enable_reloading = false
   config.eager_load = false
   config.cache_store = :null_store
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = true
 end
--- spec/mailers/task_mailer_spec.rb
+++ spec/mailers/task_mailer_spec.rb
@@ -1,32 +1,54 @@
 require "rails_helper"
 
 RSpec.describe TaskMailer, type: :mailer do
+
+  before {ActionMailer::Base.deliveries.clear}
+  let(:user) {create(:user)}
+  let(:task) { create(:task , user:user)}
+
   describe "task_created" do
-    let(:mail) { TaskMailer.task_created }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Task created")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+    let(:mail) {TaskMailer.task_created(task)}
+
+    it"sends email to correct email" do
+      expect(mail.to).to eq([user.email])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "renders the correct subject" do
+      expect(mail.subject).to eq("New Task Created: #{task.title}")
     end
+
+    it "renders the task title in the body" do
+      expect(mail.body.encoded).to include(task.title)
+    end
+
+    it "renders the task priority oin the body" do
+      expect(mail.body.encoded).to include(task.priority)
+    end
+
+
   end
 
   describe "task_due_reminder" do
-    let(:mail) { TaskMailer.task_due_reminder }
+    let(:mail) { TaskMailer.task_due_reminder(task) }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Task due reminder")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+    it "sends to correct receipient" do
+      expect(mail.to).to eq([user.email])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "renders the correct subject" do
+      expect(mail.subject).to eq("Reminder: #{task.title} is due soon!")
     end
+
+    it "renders the task title in the body" do
+      expect(mail.body.encoded).to include(task.title)
+    end
+
+    it "has an attachment" do
+      expect(mail.attachments.count).to eq(1)
+      expect(mail.attachments.first.filename).to eq('task_details.txt')
+    end
+
   end
 
 end
